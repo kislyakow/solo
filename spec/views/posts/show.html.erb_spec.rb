6dbf1c80b5f16ffe6b7b2d784f9c74b0caec53d1
Sergey Kislyakov <kislyakow@gmail.com>
2014-05-25 12:08:45 +0400

    scaffold post

---
+++ spec/views/posts/show.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "posts/show", :type => :view do
+  before(:each) do
+    @post = assign(:post, Post.create!(
+      :title => "Title",
+      :body => "MyText"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/MyText/)
+  end
+end
