9d350472a879e1c44e9adaf672b9786d4a2cc182
Sergey Kislyakov <kislyakow@gmail.com>
2014-05-25 15:44:04 +0400

    presence_of validations for title and body

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Post, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it { should validate_presence_of(:title) }
+	it { should validate_presence_of(:body) }
 end
