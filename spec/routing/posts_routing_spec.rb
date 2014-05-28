99b3c76c464a1b820a6ca4b367a49b026a48ac5c
Sergey Kislyakov <kislyakow@gmail.com>
2014-05-28 21:33:56 +0400

    rewrite posts routing specs

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -3,33 +3,13 @@ require "spec_helper"
 describe PostsController, :type => :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/posts").to route_to("posts#index")
-    end
-
-    it "routes to #new" do
-      expect(:get => "/posts/new").to route_to("posts#new")
-    end
-
-    it "routes to #show" do
-      expect(:get => "/posts/1").to route_to("posts#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/posts/1/edit").to route_to("posts#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/posts").to route_to("posts#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/posts/1").to route_to("posts#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/posts/1").to route_to("posts#destroy", :id => "1")
-    end
+    it { should route(:get, '/posts').to(controller: 'posts', action: 'index') }
+    it { should route(:get, '/posts/new').to(controller: 'posts', action: 'new') }
+    it { should route(:get, '/posts/1').to(controller: 'posts', action: 'show', id: '1') }
+    it { should route(:get, '/posts/1/edit').to(controller: 'posts', action: 'edit', id: '1') }
+    it { should route(:post, '/posts').to(controller: 'posts', action: 'create') }
+    it { should route(:put, '/posts/1').to(controller: 'posts', action: 'update', id: '1') }
+    it { should route(:delete, '/posts/1').to(controller: 'posts', action: 'destroy', id: '1') }
 
   end
 end
