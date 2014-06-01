a36142ba668323d1a11266a9925df8eabb613610
Sergey Kislyakov <kislyakow@gmail.com>
2014-06-01 11:04:45 +0400

    simple url for login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'blog/index'
 
   mount RedactorRails::Engine => '/redactor_rails'
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :posts
 
   root 'blog#index'
