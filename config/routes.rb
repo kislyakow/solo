0526dfb76cd31be834d4fbc5963106772e86ec72
Sergey Kislyakov <kislyakow@gmail.com>
2014-06-01 12:36:46 +0400

    rout for signup on

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'blog/index'
 
   mount RedactorRails::Engine => '/redactor_rails'
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  #devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users
   resources :posts
 
   root 'blog#index'
