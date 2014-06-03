41bce068cc4bf989e1e502889a25b57ff61d9a86
Sergey Kislyakov <kislyakow@gmail.com>
2014-06-03 23:11:45 +0400

    cleaning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
-Rails.application.routes.draw do  
-
-  mount Ckeditor::Engine => '/ckeditor'
+Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   get 'blog/index'
-
-  #devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
-  devise_for :users
-
-  resources :posts do
-    resources :comments
-  end
-
+  get 'posts/index', as: 'user_root'
   match '/404', :to => 'errors#not_found', :via => [:get]
   match '/500', :to => 'errors#internal_error', :via => [:get]
   match '/422', :to => 'errors#unprocessable_entity', :via => [:get]
-
-  root 'blog#index'
-
-  get 'posts/index', as: 'user_root'
+  mount Ckeditor::Engine => '/ckeditor'  
+  resources :posts do
+    resources :comments
+  end
+  root 'blog#index'  
 end
