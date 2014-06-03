f2898177bc54c583571f3259a1644e7aa43fef01
Sergey Kislyakov <kislyakow@gmail.com>
2014-06-03 23:02:00 +0400

    custom gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 	def avatar_url(comment)
+		default_url = "#{root_url}public/guest.png"
     gravatar_id = Digest::MD5::hexdigest(comment.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
 end
