574ec97eab18fe263c69a8dbab497e0b5233885c
Sergey Kislyakov <kislyakow@gmail.com>
2014-06-03 21:07:21 +0400

    cleaning controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,34 +2,26 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, except: [:show]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all.order("updated_at DESC")
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
     @post = Post.where(:published => true).find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.json { render json: @post }
     end
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
 
@@ -44,8 +36,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -58,8 +48,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -69,12 +57,10 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :body, :published)
     end
